396f606ef5926693f996e1ec19fe138a9870bd7a
Vlad Ryaboy <ryaboyvlad1712@mail.ru>
2022-04-11 17:10:12 +0700

    Da

--- RMP_PR11/View/Windows/MenuSystem.xaml.cs
+++ RMP_PR11/View/Windows/MenuSystem.xaml.cs
@@ -9,8 +9,6 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using System.Diagnostics;
 using RMP_PR11.View.Pages;
 
