c73ade1c6c00fc623a62c94ee5f727a9013fe824
Saggitarius <ryaboyvlad1712@mail.ru>
2022-04-13 13:40:51 +0700

    Full PR-13

--- AddDataToDB/Program.cs
+++ AddDataToDB/Program.cs
@@ -1,13 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using AddDataToDB.LocalModel;
 using LibForClasses;
-using RMP_PR11.Model;
-using RMP_PR11.View;
 
 namespace AddDataToDB
 {
@@ -15,16 +9,15 @@ namespace AddDataToDB
     {
         static void Main(string[] args)
         {
-            bool chek = true;
-            AirlineEntities db = DBConnect.GetContext();
- /*           int[] ids = new int[db.Users.ToList().Count];
-            string[] logins = new string[db.Users.ToList().Count];*/
+            AirEntities db = new AirEntities();
+            int[] ids = new int[db.Users.ToList().Count];
+            string[] logins = new string[db.Users.ToList().Count];
             string name, surname, login, password, role;
-           /* for (int i = 1; i <= ids.Length; i++)
+            for (int i = 1; i <= ids.Length; i++)
             {
                 ids[i - 1] = Convert.ToInt32(db.Users.ToList().Find(x => x.UserID == i).UserID);
                 logins[i - 1] = db.Users.ToList().Find(x => x.UserID == i).Login;
-            }*/
+            }
 
             Console.Write("Создание новой записи для пользователя\n");
             Console.Write("Введите имя пользователя: ");
@@ -33,24 +26,23 @@ namespace AddDataToDB
             surname = Convert.ToString(Console.ReadLine());
             Console.Write("Введите логин пользователя: ");
             login = Convert.ToString(Console.ReadLine());
-            /*do
+            bool chek = true;
+            do
             {
-                chek = true;
-                for(int i = 0; i<logins.Length; i++)
+                for (int i = 0; i < logins.Length; i++)
                 {
-                    if(login == logins[i])
+                    if (login == logins[i])
                     {
-                        chek = false;
                         Console.Write("Такой логин уже существует! Придумайте другой логин: ");
                         login = Convert.ToString(Console.ReadLine());
                         Console.WriteLine();
-                    } 
+                    }
+                    else
+                    {
+                        chek = false;
+                    }
                 }
-                if (chek == true)
-                {
-                    break;
-                }
-            } while (true);*/
+            } while (chek);
             Console.Write("Введите пароль пользователя: ");
             password = Convert.ToString(Console.ReadLine());
             DataHashing dataHashing = new DataHashing();
@@ -59,9 +51,9 @@ namespace AddDataToDB
             Console.Write("\nВведите роль пользователя (U / A): ");
             role = Convert.ToString(Console.ReadLine());
             Console.WriteLine("\nУчетная запись добавлена ");
-            Users user = new Users()
+            LocalModel.Users user = new LocalModel.Users()
             {
-                UserID = 1, //ids.Max()+1,
+                UserID = ids.Max()+1,
                 Name = name,
                 Surname = surname,
                 Login = login,
--- RMP_PR11/View/Pages/UsersPage.xaml.cs
+++ RMP_PR11/View/Pages/UsersPage.xaml.cs
@@ -1,20 +1,10 @@
 ﻿using RMP_PR11.LocalClasses;
 using RMP_PR11.Model;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace RMP_PR11.View.Pages
 {
@@ -32,20 +22,26 @@ namespace RMP_PR11.View.Pages
 
         private void AddNewUser_Click(object sender, RoutedEventArgs e)
         {
-            Process.Start(@"E:\Other\repos\RMP_PR11_rya\AddDataToDB\bin\Debug\AddDataToDB.exe");
+            Process.Start(@"C:\Users\mobil\Desktop\Kataev_Practice-master\AddDataToDB\bin\Debug\AddDataToDB.exe");
             if(check())
             {
                 UsersLV.ItemsSource = db.Users.ToList();
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
             }
         }
 
         public bool check()
         {
-            foreach (Process process in Process.GetProcessesByName("AddDataToDB.exe"))
+            foreach (Process process in Process.GetProcessesByName("AddDataToDB"))
             {
-                return true;
+                if (process.ProcessName.ToString() == "AddDataToDB")
+                {
+                    GC.Collect();
+                    GC.WaitForPendingFinalizers();
+                    check();
+                }
             }
-            check();
             return true;
 
         }
